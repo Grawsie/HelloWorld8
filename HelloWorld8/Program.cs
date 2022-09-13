921a34a7e69d3ee6dda96fe5c59634dd572f7d79
Grawsie <ryanhockey@live.ca>
2022-09-13 09:06:09 -0300

    Initial commit

---
+++ HelloWorld8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World EIGHT!!!");
+            Console.WriteLine("How many more? LOL");
+            Console.ReadKey(true);
+
+        }
+    }
+}
